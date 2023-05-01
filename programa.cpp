e3f31b2bab43284c0d8ad8aea549efa1f767833b
Raphael Laurentino da Silva <raphael.tw22@gmail.com>
2023-05-01 19:14:16 -0300

    v5-Resumo da aplicação

--- programa.cpp
+++ programa.cpp
@@ -1,45 +1,77 @@
 #include <iostream>
-#include <fstream>
-#include <GeoIP.h>
+#include <string>
+#include <curl/curl.h>
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
 
-int main() {
-    // Acessando a localização do dispositivo
-    const char* dbfile = "/path/to/GeoIP.dat"; // Substitua pelo caminho do arquivo GeoIP no seu sistema
-    GeoIP* gi = GeoIP_open(dbfile, GEOIP_STANDARD);
-    GeoIPRecord* gir = GeoIP_record_by_addr(gi, "IP_DO_DISPOSITIVO"); // Substitua "IP_DO_DISPOSITIVO" pelo endereço IP do dispositivo que você deseja monitorar
-    double latitude = gir->latitude;
-    double longitude = gir->longitude;
-    GeoIPRecord_delete(gir);
-    GeoIP_delete(gi);
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    return size * nmemb;
+}
 
-    // Tirando uma foto usando a câmera frontal do dispositivo
-    VideoCapture cap(1); // Seleciona a câmera frontal com índice 1
-    cap.set(CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G')); // Define o formato de vídeo para MJPG
+int main(int argc, char** argv) {
+    // Configurar informações do e-mail
+    string from = "seuemail@gmail.com";
+    string password = "suasenha";
+    string to = "destinatario@gmail.com";
+    string subject = "Assunto do E-mail";
+    string body = "Corpo do E-mail";
+
+    // Capturar a localização do dispositivo
+    CURL *curl;
+    CURLcode res;
+    curl = curl_easy_init();
+    string location;
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.ipify.org");
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &location);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+
+    // Capturar a foto da câmera frontal
+    VideoCapture cap(0, CAP_V4L2);
+    if (!cap.isOpened()) {
+        cerr << "Erro ao acessar a câmera." << endl;
+        return -1;
+    }
+    cap.set(CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G'));
+    cap.set(CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(CAP_PROP_FRAME_HEIGHT, 480);
     Mat frame;
-    cap >> frame; // Captura o frame atual da câmera
-    
-    // Salvando a foto em um arquivo
+    cap.read(frame);
+
+    // Enviar a foto capturada por e-mail
     string filename = "foto.jpg";
-    imwrite(filename, frame); // Salva o frame em um arquivo
-
-    // Escrevendo a localização e o nome do arquivo em um arquivo de texto
-    ofstream myfile;
-    myfile.open("localizacao.txt");
-    myfile << "Latitude: " << latitude << "\n";
-    myfile << "Longitude: " << longitude << "\n";
-    myfile << "Foto: " << filename << "\n";
-    myfile.close();
-    
-    // Exibindo a foto
-    namedWindow("Foto", WINDOW_NORMAL);
-    imshow("Foto", frame);
-    waitKey(0);
-
+    imwrite(filename, frame);
+    curl = curl_easy_init();
+    if (curl) {
+        struct curl_slist *recipients = NULL;
+        string upload_url = "smtps://smtp.gmail.com:465";
+        string upload_userpass = from + ":" + password;
+        string upload_payload = "Subject: " + subject + "\r\n"
+                                "To: " + to + "\r\n"
+                                "\r\n"
+                                + body + "\r\n";
+        curl_easy_setopt(curl, CURLOPT_URL, upload_url.c_str());
+        curl_easy_setopt(curl, CURLOPT_USERPWD, upload_userpass.c_str());
+        curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
+        curl_easy_setopt(curl, CURLOPT_MAIL_FROM, from.c_str());
+        recipients = curl_slist_append(recipients, to.c_str());
+        curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+        struct curl_httppost *formpost = NULL;
+        struct curl_httppost *lastptr = NULL;
+        curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "file",
+                     CURLFORM_FILE, filename.c_str(), CURLFORM_END);
+        curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        curl_formfree(formpost);
+        curl_slist_free_all(recipients);
+    }
+    remove(filename.c_str());
     return 0;
 }
